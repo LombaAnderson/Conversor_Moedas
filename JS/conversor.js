1b2d541c6a0bccce7c0eda0374b1c23a035852b4
Anderson Lomba <andersonlomba172@gmail.com>
2021-03-26 13:32:01 -0300

    Simples conversor de moedas feito com HTML, CSS e Javascript

---
+++ JS/conversor.js
@@ -0,0 +1,13 @@
+
+  
+  var valorEmDolarTexto = prompt("Qual o valor em dolar que você quer converter? ")
+
+  var valorEmDolarNumero = parseFloat (valorEmDolarTexto)
+  
+
+  
+  var valorEmReal = valorEmDolarNumero * 5.71
+  var valorEmRealFixado = valorEmReal.toFixed(2)
+  
+  alert(valorEmRealFixado)
+
